5d42550b06da12ddd4f0a2ebf3051f35cdd770fd
magraz <agrazvallejo@live.com>
2021-09-04 21:41:14 -0500

    stable v1

--- program_a.cpp
+++ program_a.cpp
@@ -2,18 +2,55 @@
 #include <fstream>
 #include <random>
 #include <bits/stdc++.h>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+
 #include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 
 using namespace std;
 
-int main(int argc, char **argv){
-    //Get log file path from parameters
-    char* logFilePath {argv[argc-1]};    
-    cout << "Log File Path: "<< logFilePath << endl;
+//*Constants
+const int PORT = 8080;
+const int MAX_ARR_SIZE = 10;
 
-    //Open log file
+int main(int argc, char **argv){
+
+    ////* ARGUMENTS CHECK *////
+
+    //Check the number of arguments
+    if (argc > 3){
+        cerr << "Too many arguments!" << endl;
+        return -1;
+    }
+    else if (argc == 1){
+        cout << "Usage './program_a <'logFileName'> <'-a' to append or leave empty to overwrite>'" << endl;
+        return 0;
+    }
+
+    ////* LOG FILE SETUP *////
+
+    //Get log file path from parameters
+    char *logFilePath{argc == 3 ? argv[argc - 2] : argv[argc - 1]};
+
+    //Determine how to open logfile if in append or overwrite mode(this is the default mode)
     ofstream logFile;
-    logFile.open(logFilePath);
+    if(argc == 3){
+        char *openMode = argv[argc - 1];
+        if ((openMode[0] == '-') && (openMode[1] == 'a')) logFile.open(logFilePath, ios::app);
+        else {
+            cerr << "Invalid open mode!" << endl;
+            return -1;
+        };
+        
+    }
+    else logFile.open(logFilePath, ios::trunc);
+
+    cout << "Log file path: " << logFilePath << endl;
+
+    ////* REQUEST GENERATION *////
 
     //Generate random number list
     random_device rd;                         // obtain a random number from hardware
@@ -21,22 +58,63 @@ int main(int argc, char **argv){
     uniform_int_distribution<> distr(1, 20); // define the range
     vector<int> randomNumberList{};
 
-    for (int n = 0; n < 10; ++n) randomNumberList.push_back(distr(gen)); // generate numbers
+    for (int n = 0; n < MAX_ARR_SIZE; ++n) randomNumberList.push_back(distr(gen)); // generate numbers
 
     //Sort the numbers in ascending order
     sort(randomNumberList.begin(), randomNumberList.end());
 
-    for(auto x : randomNumberList){
-        logFile << x << endl;
+    //Convert vector<int> to string
+    stringstream ss;
+    copy(randomNumberList.begin(), randomNumberList.end(), ostream_iterator<int>(ss, ",")); //copy int to string, and format as comma separated values
+    string data_str = ss.str();
+    data_str = data_str.substr(0, data_str.length() - 1); // get rid of the trailing space
+
+    //Log and setup data to send
+    cout << "Sending data: " << data_str << endl;
+
+    ////* SOCKET CLIENT SETUP *////
+
+    int sock = 0, valread;
+    struct sockaddr_in serv_addr;
+    char *data = &*data_str.begin(); //get pointer to first char in data_str
+    char buffer[1024] = {0};
+
+    //Create socket
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+        printf("\n Socket creation error \n");
+        return -1;
     }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0){
+        cout << "Invalid address/ Address not supported" << endl; 
+        return -1;
+    }
+
+    //Perform connection
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0){
+        cout << "Transmission failed!" << endl;
+        logFile << "Transmission failed!" << endl;
+        return -1;
+    }
+
+    //Send data
+    send(sock, data, strlen(data), 0);
+    cout << "Data sent: " << data << endl;
+    valread = read(sock, buffer, 1024);
+    cout << "Transmisssion succeded message from server: " << buffer << endl; //Should read "Transmisssion succeded message from server: XXXX" when connection succeeds
+    logFile << "Transmisssion succeded message from server: " << buffer << endl;
+
+    //Get exit key press and log it
+    cout << "Enter any key to exit" << endl;
+    char exitKey = getchar();
+    logFile << "Program finished" << endl << "Exit key: " << exitKey;
 
     //Close log file
     logFile.close();
 
-    //Socket Server 
-    //int server, new_socket, valread;
-    //struct sockadd
-
-    //logFile << data << endl;
-
+    return 0;
 }
--- program_b.cpp
+++ program_b.cpp
@@ -1,59 +1,139 @@
 #include <iostream>
 #include <fstream>
-#include <sys/socket.h>
 #include <sstream>
 #include <bits/stdc++.h>
 
+#include <sys/socket.h>
+#include <unistd.h>
+#include <netinet/in.h>
+
 using namespace std;
 
+//*Constants
+const uint64_t FACTORIAL_LIMIT = 100000000000;
+const int PORT = 8080;
+
+//*Functions
 uint64_t factorial(uint64_t num){
     if(num == 1) return 1;
     else return num * factorial(num-1);
 }
+vector<int> split(const string &s, char delimiter){
+    vector<int> tokens;
+    string token;
+    istringstream tokenStream(s);
+    while (getline(tokenStream, token, delimiter)){
+        tokens.push_back(stoi(token));
+    }
+    return tokens;
+}
 
-int main(int argc, char **argv)
-{
-    //Get log file path from parameters
-    char *logFilePath{argv[argc - 1]};
-    cout << "Log File Path: " << logFilePath << endl;
+int main(int argc, char **argv){
+    ////* ARGUMENTS CHECK *////
 
-    //Open log file
-    ofstream logFile;
-    logFile.open(logFilePath);
-
-    ifstream inputFile;
-    inputFile.open("holi.dat");
-
-    char data[10] {0};
-    cout << "Reading from the file" << endl;
-
-    vector<uint64_t> data_arr {};
-
-    logFile << "Received data: " << endl;
-    if(inputFile.is_open()){
-        string data;
-        while(getline(inputFile, data)){
-            logFile << data << endl; //log received data
-            
-            //Convert string to int
-            stringstream data_str(data);
-            uint64_t data_int{};
-            data_str >> data_int;
-
-            //Add processed integers to array
-            uint64_t data_fact {factorial(data_int)}; //calculate factorial
-            if (data_fact < 100000000000) data_arr.push_back(data_fact);
-        }
+    //Check the number of arguments
+    if (argc > 3){
+        cerr << "Too many arguments!" << endl;
+        return -1;
+    }
+    else if (argc == 1){
+        cout << "Usage './program_a <'logFileName'> <'-a' to append or leave empty to overwrite>'" << endl;
+        return 0;
     }
 
+    ////* LOG FILE SETUP *////
+
+    //Get log file path from parameters
+    char *logFilePath{argc == 3 ? argv[argc - 2] : argv[argc - 1]};
+
+    //Determine how to open logfile if in append or overwrite mode(this is the default mode)
+    ofstream logFile;
+    if (argc == 3){
+        char *openMode = argv[argc - 1];
+        if ((openMode[0] == '-') && (openMode[1] == 'a'))
+            logFile.open(logFilePath, ios::app);
+        
+        else{
+            cerr << "Invalid open mode!" << endl;
+            return -1;
+        };
+    }
+    else logFile.open(logFilePath, ios::trunc);
+
+    cout << "Log file path: " << logFilePath << endl;
+
+    ////* SOCKET SERVER SETUP *////
+
+    int server_fd, new_socket, valread;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[1024] = {0};
+    const char *response = "DONE!";
+
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0){
+        cerr << "Socket failed";
+        return -1;
+    }
+
+    // Forcefully attaching socket to the port 8080
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))){
+        cerr << "Set socket failed";
+        return -1;
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    // Forcefully attaching socket to the port 8080
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0){
+        cerr << "Bind failed";
+        return -1;
+    }
+    if (listen(server_fd, 3) < 0){
+        cerr << "Listening failed";
+        return -1;
+    }
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0){
+        cerr << "Connection not accepted";
+        return -1;
+    }
+
+    //Read incoming request from client
+    valread = read(new_socket, buffer, 1024);
+    cout << "Request received: " << buffer << endl;
+    logFile << "Request received: " << buffer << endl;
+
+    //Send response to client
+    send(new_socket, response, strlen(response), 0);
+    cout << "Response sent: " << response << endl;
+    
+    ////* PROCESS RECEIVED DATA *////
+    
+    //Convert received data to string, and to vector afterwards
+    string data_str(buffer); 
+    vector<int> data_vect {split(data_str, ',')}; //input vector
+    vector<uint64_t> result{}; //output vector
+
+    //Run every element of the vector through the factorial function
+    for(int n = 0; n < data_vect.size(); n++){
+        uint64_t data_fact{factorial(data_vect[n])}; //calculate factorial
+        if (data_fact < FACTORIAL_LIMIT) result.push_back(data_fact); //If factorial is less than 100 000 000 000 add it to the result vector 
+    }
+    
+    //Log output
     logFile << "Processed data: " << endl;
-    for(auto x : data_arr) logFile << x << endl;
+    for(auto x : result) logFile << x << endl;
 
+    //Get exit key press and log it
+    cout << "Enter any key to exit" << endl;
+    char exitKey = getchar();
+    logFile << "Program finished." << endl << "Exit key: " << exitKey;
+    
+    //Close log file
     logFile.close();
-    inputFile.close();
-    //Socket Server
-    //int server, new_socket, valread;
-    //struct sockadd
 
-    //logFile << data << endl;
+    return 0;
 }
